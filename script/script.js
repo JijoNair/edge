ef8c7529d39f7048a7a8d554c923966a5b620328
JIjo Nair <jnair@gravitas.co>
2018-05-31 22:16:09 +0530

    final

--- script/script.js
+++ script/script.js
@@ -5,16 +5,25 @@ $( document ).ready(function() {
  var filenm = document.location.pathname.match(/[^\/]+$/)[0];
 console.log(filenm);
  if(filenm == "index.html"){
-  setTimeout(explode, 3000);
+  setTimeout(explode, 2000);
  }
 
 
   $('.ui.accordion').accordion();
   $('.tag.example .ui.dropdown').dropdown({allowAdditions: true});
+function showdata(){
+  $('#loader').removeClass("active")
 
+
+}
   $('#search').click(function(){
+    $('#gettdata').show();
+
+        $('#acc').transition('scale');
+        setTimeout(showdata, 1000);
+
     //$( "#acc" ).toggle( display );
-    $('#acc').transition('scale');
+
 
 });
 
@@ -22,6 +31,9 @@ $('#clear').click(function(){
   //$( "#acc" ).toggle( display );
   $('#acc').transition('scale');
   $('#txt').val("");
+    $('#gettdata').show();
+    $('#loader').addClass("active");
+
 
 });
 
@@ -31,17 +43,26 @@ $('#clear').click(function(){
 $('.info.circle.link.icon').click(function(){
   $('#showinfo').modal('show');
 });
+$('.pencil.icon').click(function(){
+  $('#comingsoon').modal('show');
+});
+$('.file.icon').click(function(){
+  $('#comingsoon').modal('show');
+});
+$('.copy.icon').click(function(){
+  $('#comingsoon').modal('show');
+});
+
 
   $('.menu .item').tab();
 
-  $('#example').DataTable( {
-       columnDefs: [
-           {
-               targets: [ 0, 1, 2 ],
-               className: 'mdl-data-table__cell--non-numeric'
-           }
-       ] });
+  $('#exampletb1').DataTable();
+  $('#exampletb2').DataTable();
 
-   $('#info').popup();
+
+   $('.info.circle.link.icon').popup();
+   $('.pencil.icon').popup();
+   $('.file.icon').popup();
+   $('.copy.icon').popup();
 
 });
