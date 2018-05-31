db5f0060c735161db91cf5d658d7e37441754032
Jijo B Nair <jijo.nair@codecyclopedia.com>
2018-05-31 16:23:09 +0530

    before-layout

--- script/script.js
+++ script/script.js
@@ -1,17 +1,39 @@
 $( document ).ready(function() {
+  function explode(){
+      window.location.href = "app.html";
+}
+ var filenm = document.location.pathname.match(/[^\/]+$/)[0];
+console.log(filenm);
+ if(filenm == "index.html"){
+  setTimeout(explode, 3000);
+ }
+
+
   $('.ui.accordion')
+
     .accordion()
   ;
   $('.tag.example .ui.dropdown').dropdown({allowAdditions: true});
 
   $('#search').click(function(){
-    //$('.ui.modal').modal('show');
-    $('.coupled.modal').modal({allowMultiple: true});
- // open second modal on first modal buttons
- $('.second.modal').modal('attach events', '.first.modal .icon');
- // show first immediately
-  $('.first.modal').modal('show');
-  });
+    //$( "#acc" ).toggle( display );
+    $('#acc').transition('scale');
+
+});
+
+$('#clear').click(function(){
+  //$( "#acc" ).toggle( display );
+  $('#acc').transition('scale');
+  $('#txt').val("");
+
+});
+
+
+
+
+$('.info.circle.link.icon').click(function(){
+  $('#showinfo').modal('show');
+});
 
   $('.menu .item').tab();
 
@@ -21,11 +43,8 @@ $( document ).ready(function() {
                targets: [ 0, 1, 2 ],
                className: 'mdl-data-table__cell--non-numeric'
            }
-       ]
-   } );
+       ] });
 
    $('#info').popup();
-
-
 
 });
